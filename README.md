dc6b11854818c2bcd6a72c2a0f92ef867bec4b88
JC98683 <jcombs@msn.com>
2026-05-05 14:00:51 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# PDX Simple Flight Data v1.2.2
+# PDX Simple Flight Data v1.2.7
 
-Full web app package.
-
-Files:
-- index.html
-- manifest.json
-- sw.js
-
-For GitHub Pages:
-1. Upload these files to the root of the repository.
-2. Enable GitHub Pages from the main branch.
-3. Open the published URL in Safari.
-4. Use Share > Add to Home Screen.
+Upload contents to GitHub Pages repo root.
--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-const CACHE_NAME = 'pdx-simple-flight-data-v1.2.2';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
+const CACHE_NAME = 'pdx-simple-flight-data-v1.2.7';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
