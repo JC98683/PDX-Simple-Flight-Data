560351623f9602b268e2ec6f737589c5eb1e0e78
JC98683 <jcombs@msn.com>
2026-05-10 14:18:39 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pdx-simple-flight-data-v1.2.7';
+const CACHE_NAME = 'pdx-simple-flight-data-v1.3.8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
@@ -9,5 +9,6 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
